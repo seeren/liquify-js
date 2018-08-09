8babf2ee78f28909fbf5493751088de8adb9f819
Cyril <consultant@seeren.fr>
2018-08-09 04:58:15 +0200

    Add main

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * liquify.js
+ *     __
+ *    / /__ __ __ __ __ __
+ *   / // // // // // // /
+ *  /_// // // // // // /
+ *    /_//_//_//_//_//_/
+ *
+ * @author Cyril <consultant@seeren.fr>
+ * @version 0.0.1
+ * 
+ */
+
+import { Liquify } from "./core/liquify";
+
+export let liquify = global.liquify = new Liquify;
