1337d61c23dc808ac012ce0cd3a29f0c00a9429c
seeren <consultant@seeren.fr>
2018-11-25 21:07:00 +0100

    Add resize methode

--- src/scenes/scene.scene.js
+++ src/scenes/scene.scene.js
@@ -5,13 +5,26 @@ export class Scene extends THREEScene {
 
     /**
      * @constructor
-     * @param {HTMLElement} element 
      * @param {Perspective} camera 
      */
-    constructor(element, camera) {
+    constructor(camera) {
         super();
         camera.position.y = 1;
         camera.lookAt(this.position);
+    }
+
+    /**
+     * @param {number} width 
+     * @param {number} height 
+     * @param {Perspective} camera 
+     * @param {string} image 
+     */
+    resize(width, height, camera, image) {
+        if (this.plane) {
+            this.remove(this.plane);
+        }
+        this.plane = new Plane(width, height, camera, image);
+        this.add(this.plane);
     }
 
 }
