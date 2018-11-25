0382d9db3b6ac85ecd78056f264d65d61d540301
seeren <consultant@seeren.fr>
2018-11-25 21:06:48 +0100

    Add texture on constructor

--- src/scenes/meshes/plane.mesh.js
+++ src/scenes/meshes/plane.mesh.js
@@ -8,40 +8,33 @@ import {
     RGBFormat
 } from 'three';
 
-// import { TextureLoader as THREETextureLoader} from 'three';
-// import { ClampToEdgeWrapping as THREEClampToEdgeWrapping} from 'three';
-// import { LinearFilter as THREELinearFilter} from 'three';
-// import { RGBFormat as THREERGBFormat} from 'three';
-
 export class Plane extends Mesh {
 
     /**
      * @constructor
-     * @param {HTMLElement} element 
+     * @param {number} width 
+     * @param {number} height 
      * @param {Camera} camera 
+     * @param {string} image 
      */
-    constructor(element, camera) {
+    constructor(width, height, camera, image) {
         super(
             new PlaneGeometry(
-                (2 * window.Math.tan((camera.fov * window.Math.PI / 180) / 2)) * camera.aspect,
+                (2 * window.Math.tan(
+                    (camera.fov * window.Math.PI / 180) / 2
+                )) * camera.aspect,
                 2 * window.Math.tan((camera.fov * window.Math.PI / 180) / 2),
-                window.Math.ceil(element.offsetWidth / 55),
-                window.Math.ceil(element.offsetHeight / 55 / 2)
+                window.Math.ceil(width / 55),
+                window.Math.ceil(height / 55 / 2)
             ),
-            new MeshBasicMaterial
+            new MeshBasicMaterial({ map: new TextureLoader().load(image) })
         );
         this.rotation.x = -Math.PI / 2;
-    }
-    /**
-     * @param {string} src 
-     */
-    setTexture(src) {
-        this.material.map = new TextureLoader().load(src);
         this.material.map.wrapS = ClampToEdgeWrapping;
         this.material.map.wrapT = ClampToEdgeWrapping;
         this.material.map.minFilter = LinearFilter;
         this.material.map.magFilter = LinearFilter;
         this.material.map.format = RGBFormat;
-        this.material.needsUpdate = true;
     }
+
 }
