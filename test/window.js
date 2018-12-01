3cab27adba8a21d2ea8ae99ba8ee87ccb0799463
seeren <consultant@seeren.fr>
2018-12-01 19:10:22 +0100

    Add methods to jsdom window

--- test/window.js
+++ test/window.js
@@ -5,11 +5,15 @@ export let window = ((() => {
     const { JSDOM } = jsdom;
     global.window = new JSDOM('<!doctype html>').window;
     global.window.Error = Error;
+    global.window.Date = Date;
     global.window.Function = Function;
     global.window.JSON = JSON;
     global.window.Object = Object;
     global.window.RegExp = RegExp;
     global.window.parseInt = parseInt;
+    global.window.parseFloat = parseFloat;
+    global.window.requestAnimationFrame = (callback) => global.window.setTimeout(callback, 10);
+    global.window.cancelAnimationFrame = (id) => global.window.clearTimeout(id);
     return global.window;
 
 })());
