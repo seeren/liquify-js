540e9e5762afe1de23de1b01b7c3483f5fea2630
seeren <consultant@seeren.fr>
2021-12-15 23:41:50 +0100

    Copy style deeply

--- src/filters/rasterize.filter.js
+++ src/filters/rasterize.filter.js
@@ -1,25 +1,36 @@
-import html2canvas from "html2canvas";
+import html2canvas from 'html2canvas';
 
-export class Rasterize {
+import { Filter } from './filter';
+
+export class RasterizeFilter extends Filter {
+
+    async resize(target, liquify) {
+        super.resize();
+        const canvas = await this.render(target, liquify);
+        return canvas;
+    }
 
     /**
-     * @param {HTMLElement} node 
-     * @param {HTMLElement} container 
-     * @param {Function} success 
+     * @param {HTMLElement} target
+     * @param {HTMLElement} liquify
      */
-    render(node, container, success) {
-        container.style.display = "none";
-        node.style.display = "";
-        html2canvas(node, {
-            logging: false,
-            backgroundColor: null
-        }).then((canvas) => {
-            success(canvas);
-            container.style.width = node.offsetWidth + "px";
-            container.style.height = node.offsetHeight + "px";
-            node.style.display = "none";
-            container.style.display = "";
-        });
+    async render(target, liquify) {
+        super.render();
+        target.style.display = '';
+        liquify.style.display = 'none';
+        const canvas = await html2canvas(target, { logging: false, backgroundColor: null });
+        const styles = window.getComputedStyle(target);
+        if ('' !== styles.cssText) {
+            liquify.style.cssText = styles.cssText;
+        } else {
+            const cssText = Array.from(styles).reduce(
+                (css, propertyName) => `${css}${propertyName}:${styles.getPropertyValue(propertyName)};`,
+            );
+            liquify.style.cssText = cssText;
+        }
+        target.style.display = 'none';
+        liquify.style.display = '';
+        return canvas;
     }
 
 }
