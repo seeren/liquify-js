bd8d7580862246b2587ecd32ed119b802cbdcc19
seeren <consultant@seeren.fr>
2021-12-15 23:42:05 +0100

    Create liquify scene

---
+++ src/scenes/liquify.scene.js
@@ -0,0 +1,36 @@
+import { Camera, Scene } from 'three';
+
+import { Plane } from './meshes/plane.mesh';
+
+export class LiquifyScene extends Scene {
+
+    #plane;
+
+    /**
+     * @param {Camera} camera
+     * @param {HTMLElement} element
+     * @param {string} src
+     */
+    constructor(camera, element, src) {
+        super();
+        camera.position.y = 1;
+        camera.lookAt(this.position);
+        this.#plane = new Plane(camera, element, src);
+        this.add(this.#plane);
+    }
+
+    get plane() {
+        return this.#plane;
+    }
+
+    /**
+     * @param {HTMLElement} element
+     * @param {string} src
+     */
+    resize(camera, element, src) {
+        this.remove(this.#plane);
+        this.#plane = new Plane(camera, element, src);
+        this.add(this.#plane);
+    }
+
+}
