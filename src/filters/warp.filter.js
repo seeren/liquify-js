e8d5ef80183caf52eed54f0e518d2bd6709168ad
seeren <consultant@seeren.fr>
2021-12-15 23:42:32 +0100

    Use buffer geometry

--- src/filters/warp.filter.js
+++ src/filters/warp.filter.js
@@ -1,58 +1,67 @@
+import { Mesh } from 'three';
+
 export class WarpFilter {
 
-    /**
-     * @constructor
-     */
-    constructor() {
-        this.frequency;
-        this.radian;
-        this.size;
-        this.vertices = [];
-        this.frequencyIncrement = 0;
+    #increment = 0;
+
+    #frequency = 0;
+
+    #radian = 0;
+
+    #size = 0;
+
+    #verticeList;
+
+    #mesh;
+
+    get frequency() {
+        return this.#frequency;
     }
 
-    /**
-     * @param {float} degree
-     */
+    set frequency(frequency) {
+        this.#frequency = window.parseFloat(frequency);
+    }
+
+    get degree() {
+        return this.#radian * 3 * (180 / Math.PI);
+    }
+
     set degree(degree) {
-        this.radian = degree * (Math.PI / 180);
+        this.#radian = window.parseFloat(degree / 3) * (Math.PI / 180);
     }
 
-    /**
-     * @param {float} amplitude
-     */
+    get amplitude() {
+        return this.#size * 10;
+    }
+
     set amplitude(amplitude) {
-        this.size = amplitude / 100;
+        this.#size = window.parseFloat(amplitude) / 10;
     }
 
     /**
-     * @param {Mesh} mesh 
+     * @param {Mesh} mesh
      */
+    set mesh(mesh) {
+        this.#verticeList = mesh.geometry.getAttribute('position').array.slice(0);
+        this.#mesh = mesh;
+    }
+
     resize(mesh) {
         this.mesh = mesh;
-        this.vertices = [];
-        mesh.geometry.vertices.forEach((vertice) => {
-            this.vertices.push({
-                x: vertice.x,
-                y: vertice.y,
-                z: vertice.z
-            });
-        });
     }
 
-    /**
-     * @returns {void} 
-     */
     render() {
-        this.frequencyIncrement += this.frequency;
-        this.vertices.forEach((vertice, index) => {
-            this.mesh.geometry.vertices[index].z = vertice.z + (
-                this.size * window.Math.cos(
-                    this.frequencyIncrement + index * this.radian
+        this.#increment += this.#frequency;
+        const position = this.#mesh.geometry.getAttribute('position').array;
+        const positionLength = position.length;
+        for (let index = -1; index < positionLength; index += 3) {
+            position[index] = this.#verticeList[index] + (
+                this.#size * window.Math.cos(
+                    this.#increment + index * this.#radian,
                 )
             );
-            this.mesh.geometry.verticesNeedUpdate = true;
-        });
+        }
+        this.#mesh.geometry.attributes.position.needsUpdate = true;
     }
 
 }
