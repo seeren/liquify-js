9200d5fbec52a225f72d19e380c213a247f1851b
seeren <consultant@seeren.fr>
2021-12-17 23:00:49 +0100

    Add clickWrap

--- src/builder/element.builder.js
+++ src/builder/element.builder.js
@@ -1,4 +1,5 @@
-import { WarpFilter } from '../filters/warp.filter';
+import { ClickWarpFilter } from '../filters/click.warp.filter';
+import { DefaultWarpFilter } from '../filters/default.warp.filter';
 
 export class ElementBuilder {
 
@@ -6,7 +7,14 @@ export class ElementBuilder {
      * @param {HTMLElement} element
      */
     build(element) {
-        element.Liquify = new WarpFilter();
+        switch (element.getAttribute('data-liquify')) {
+        case 'click':
+            element.Liquify = new ClickWarpFilter();
+            break;
+        default:
+            element.Liquify = new DefaultWarpFilter();
+            break;
+        }
         element.Liquify.frequency = element.getAttribute('data-frequency') || 3;
         element.Liquify.degree = element.getAttribute('data-degree') || 130;
         element.Liquify.amplitude = element.getAttribute('data-amplitude') || 5;
