889bc5dd41b36bac77b67058213ed430f4d9d2c8
seeren <consultant@seeren.fr>
2021-12-24 16:33:15 +0100

    Attach handler

--- src/filters/infinite.warp.filter.js
+++ src/filters/infinite.warp.filter.js
@@ -1,17 +1,26 @@
+import { EventManager } from '../events/event.manager';
 import { WarpFilter } from './warp.filter';
 
 export class InfinitetWarpFilter extends WarpFilter {
+
+    /**
+     * @param {Mesh} cameshmera
+     * @param {Camera} camera
+     */
+    setGeometry(mesh, camera) {
+        super.setGeometry(mesh, camera);
+        const animation = EventManager.get('animation');
+        animation.detach(this.handler).attach(this.handler);
+    }
 
     render() {
         this.increment += this.frequency;
         const position = this.mesh.geometry.getAttribute('position').array;
         const positionLength = position.length;
-        for (let index = -1; index < positionLength; index += 3) {
-            position[index] = this.verticeList[index] + (
-                this.size * window.Math.cos(
-                    this.increment + index * this.radian,
-                )
-            );
+        for (let index = 0; index < positionLength; index += 3) {
+            const z = index + 2;
+            position[z] = this.verticeList[z]
+                        + (this.size * window.Math.cos(this.increment + index * this.radian));
         }
         this.mesh.geometry.attributes.position.needsUpdate = true;
     }
