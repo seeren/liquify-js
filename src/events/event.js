cac9ed2e40493c113a93c9c903ef6f80dcc82fbb
seeren <consultant@seeren.fr>
2021-12-24 16:32:30 +0100

    Fluent methods

--- src/events/event.js
+++ src/events/event.js
@@ -11,6 +11,7 @@ export class Event {
      */
     attach(callable) {
         this.#callables.push(callable);
+        return this;
     }
 
     /**
@@ -21,15 +22,18 @@ export class Event {
         if (-1 < key) {
             this.callables.splice(key, 1);
         }
+        return this;
     }
 
     notify() {
         this.#callables.forEach((callable) => callable());
+        return this;
     }
 
     clear() {
         this.#callables.splice(0);
         this.unregister();
+        return this;
     }
 
 }
