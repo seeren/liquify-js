dea80e358cbc99b2040be41ca3f9146184cd105c
seeren <consultant@seeren.fr>
2021-12-30 20:12:44 +0100

    Export Liquify

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { Liquify } from './liquify';
 import './assets/demo.jpg';
 import './assets/favicon.ico';
 
+export { Liquify } from './liquify';
+
 window.document.addEventListener('readystatechange', () => {
     if ('complete' === window.document.readyState) {
         global.Liquify = new Liquify();
